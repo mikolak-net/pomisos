909cac10db5dcafc18829900bf688d020c691e91
Bartosz Andrzejczak <bartosz.andrzejczak@softwaremill.com>
2016-11-18 16:25:28 +0100

    Added handling for all last values being the same

--- src/main/scala/net/mikolak/pomisos/quality/ConsecutiveQuality.scala
+++ src/main/scala/net/mikolak/pomisos/quality/ConsecutiveQuality.scala
@@ -5,28 +5,40 @@ import java.time.Instant
 import gremlin.scala.ScalaGraph
 import gremlin.scala._
 import smile.regression._
+import ConsecutiveQuality._
 
 class ConsecutiveQuality (db: () => ScalaGraph) {
   def predict(): Option[Double] = {
-    val last5Qualities = db().V
+    val lastQualities = db().V
         .hasLabel[PomodoroQuality]
         .toCC[PomodoroQuality]
         .toList
         .sortBy(_.timestamp)(Ordering.fromLessThan(_ isBefore _))
-        .takeRight(5)
-    if (last5Qualities.isEmpty) {
+        .takeRight(LastQualitiesCount)
+    if (lastQualities.isEmpty) {
       None
     } else {
-      val OLS = ols(
-        last5Qualities.map(a => Array(a.timestamp.getEpochSecond.toDouble)).toArray,
-        last5Qualities.map(_.quality.toDouble).toArray
-      )
-      Some(
-        OLS.predict(
-          Array(Instant.now().getEpochSecond.toDouble)
+      //TODO clean this up
+      if (lastQualities.map(_.quality).distinct.length == 1) {
+        Some(lastQualities.head.quality)
+      } else {
+        val OLS = ols(
+          lastQualities.map(a => Array(a.timestamp.getEpochSecond.toDouble)).toArray,
+          lastQualities.map(_.quality.toDouble).toArray
         )
-      )
+        Some(
+          OLS.predict(
+            Array(Instant.now().getEpochSecond.toDouble)
+          )
+        )
+      }
     }
 
+
+
   }
+}
+
+object ConsecutiveQuality {
+  val LastQualitiesCount = 5
 }
